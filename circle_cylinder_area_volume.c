8fc39ff70a2b3865f8bd53f3d83c8248820f0157
Bharti <160905596+Bharti-02@users.noreply.github.com>
2025-06-19 22:05:33 +0530

    Add files via upload
    C program to calculate the area of a circle and the volume of a cylinder using the given radius and height.

---
+++ circle_cylinder_area_volume.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int radius = 7;
+    float pie = 3.14;
+    printf("the area of this circle %f \n", pie * radius * radius);
+    int height = 3;
+    printf("the volume of cyclinder is %f \n", pie * radius * radius * height);
+
+    return 0;
+}
