bc8a19923d7ff66d051571a87003498387b9757d
Bharti <160905596+Bharti-02@users.noreply.github.com>
2025-06-19 22:03:53 +0530

    Add files via upload
    C program to check inequality using != operator and print the result (1 for true, 0 for false).

---
+++ relational_op.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/*this is our c first program 
+which is awsome!
+*/
+int main(){
+    int a = 3,b=2,c=3,results;
+    results = (a!=b);
+    
+    
+     
+     printf("%d\n",results);
+
+
+    return 0;
+}
+
+
+   
+    
