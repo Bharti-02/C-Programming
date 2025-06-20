7b0078855c3db4f926cf31e5084c7964b38cf6c1
Bharti <160905596+Bharti-02@users.noreply.github.com>
2025-06-20 07:59:11 +0530

    Add files via upload
    C program to print elements of an integer array using a loop

---
+++ array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int i ;
+    int arr[]={1,2,3,4,5};
+    printf("elements of the given array :");
+    for(i=0;i<=5;++i){
+        printf("%d",arr[i]);
+
+    }
+    return 0;
+}
