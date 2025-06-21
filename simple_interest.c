4bc800ca5438c57d2ba91b60ac8b01d2e2c4a77f
Bharti <160905596+Bharti-02@users.noreply.github.com>
2025-06-21 10:21:05 +0530

    Add files via upload
    This C program calculates the Simple Interest (SI).

---
+++ simple_interest.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int principal=100,rate=5 ,years=1 ;
+    int simpleIntrest = (principal * rate * years)/100;
+    printf("the value of simple intrest %d",simpleIntrest);
+
+    return 0;
+}
